036213469d276ced1582e81de3ce9104f48fc474
admin-johan <johan.van.wyk@capgemini.com>
2025-05-01 10:44:56 +0200

    Add dark version

--- qr/qr.js
+++ qr/qr.js
@@ -2,10 +2,20 @@ const QRCode = require('qrcode');
 
 const url = 'https://github.com/Druaka';
 
-QRCode.toFile('qr_Druaka.png', url, {
+QRCode.toFile('qr_Druaka-light.png', url, {
     color: {
         dark: '#000000',  // QR code color
         light: '#FFFFFF'  // Background color
+    }
+}, function (err) {
+    if (err) throw err;
+    console.log('QR code generated!');
+});
+
+QRCode.toFile('qr_Druaka-dark.png', url, {
+    color: {
+        dark: '#FFFFFF',  // QR code color
+        light: '#000000'  // Background color
     }
 }, function (err) {
     if (err) throw err;
